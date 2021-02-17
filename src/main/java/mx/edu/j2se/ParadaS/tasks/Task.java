36c2db676f9fc5c4b9906c966af8dc91a450d19a
edpasa23 <edpasa2305@gmail.com>
2021-02-17 13:17:51 -0600

    TAREA 1

--- src/main/java/mx/edu/j2se/ParadaS/tasks/Task.java
+++ src/main/java/mx/edu/j2se/ParadaS/tasks/Task.java
@@ -5,4 +5,26 @@ package mx.edu.j2se.ParadaS.tasks;
 //Clase Task
 public class Task {
 
+    //Atributos
+
+    String title;
+    int time;
+    int start;
+    int end;
+    int interval;
+
+
+    //Métodos
+
+    //Contructor para tarea no repetitiva
+    public Task (String title, int time){
+
+    }
+
+    //Contructor para tarea repetitiva
+    public Task (String title, int start, int end, int interval){
+
+    }
+
+
 }
