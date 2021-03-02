b63e305b575cf7346053d823b3094babdbec5d4d
edpasa23 <edpasa2305@gmail.com>
2021-03-02 13:39:37 -0600

    TAREA 2

---
+++ src/main/java/mx/edu/j2se/ParadaS/tasks/ArrayTaskList.java
@@ -0,0 +1,68 @@
+package mx.edu.j2se.ParadaS.tasks;
+
+/**
+ * class ArrayTaskList
+ * @version 1.0 23 Feb 2021
+ * @author Eduardo Parada
+ */
+
+public class ArrayTaskList {
+
+    // Atributtes
+
+    Task[] taskList;
+    int last = 0;
+    int current = 0;
+
+    // Methods
+
+    // Method to add an specified task to the list
+    public void add (Task task){
+        taskList[last] = task;
+        last++;
+    }
+
+    // Method to remove a task (if it exist)
+    public boolean remove (Task task){
+        for(current = 0 ; current < last ; current++ ){
+            if (task==taskList[current]){
+                for(int j = current + 1; j < last ; j++){
+                    taskList[current]=taskList[j];
+                }
+                last = last - 1;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Method that returns the number of tasks
+    public int size(){
+        return last;
+    }
+
+    // Method that return an specified task in the list
+    public Task getTask(int index) {
+        return taskList[index];
+    }
+
+    // Method that shows tasks that are in a specific period of time
+    public ArrayTaskList incoming(int from, int to){
+
+        ArrayTaskList searchArray = new ArrayTaskList();
+
+        for(current = 0; current < last; current++){
+            if(taskList[current].active == true) {
+                if (taskList[current].start > from && taskList[current].end < to) {
+                    searchArray.add(taskList[current]);
+                }
+                else if (taskList[current].time > from && taskList[current].time < to){
+                    searchArray.add(taskList[current]);
+                }
+            }
+        }
+        return searchArray;
+    }
+
+}
+
